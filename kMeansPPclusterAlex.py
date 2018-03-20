9f146dde6ec900afb9b27c4220a64ef1b820ddf6
apalom <35828580+apalom@users.noreply.github.com>
2018-03-20 13:06:47 -0600

    Data Normalization

--- kMeansPPclusterAlex.py
+++ kMeansPPclusterAlex.py
@@ -18,21 +18,27 @@ import timeit
 
 start_time = timeit.default_timer()
 
-clusters = 5
+clusters = 4
+numTrips = 500
+colNames = ['STRTTIME','TRVLCMIN']
+# Filter dataframe zero (raw NHTS2009) to dimensions of interest
+#dfNHTS = df0.filter(['STRTTIME','TRVL_MIN','WHYTRP1S'], axis = 1)
+dfNHTS = df0.filter(colNames, axis = 1)
+dfNHTS = dfNHTS.head(numTrips) # shows first n rows
 
-# filter dataframe zero (raw NHTS2009) to dimensions of interest
-#dfAlex = df0.filter(['STRTTIME','TRVL_MIN','WHYTRP1S'], axis = 1)
-dfAlex = df0.filter(['STRTTIME','WHYTO'], axis = 1)
-dfAlex = dfAlex.head(1000) # shows first n rows
+# Normalize dataframe by max value in each row
+dfNorm = dfNHTS
+dfNorm.iloc[:,0] = dfNHTS.iloc[:,0]/dfNHTS.max()[0]
+dfNorm.iloc[:,1] = dfNHTS.iloc[:,1]/dfNHTS.max()[1]
 
-
-kmeans = KMeans(n_clusters=clusters, init='k-means++', n_init=10, max_iter=300).fit(dfAlex)
+# Calculate k-Means
+kmeans = KMeans(n_clusters=clusters, init='k-means++', n_init=10, max_iter=300).fit(dfNHTS)
 
 phi_true = kmeans.labels_
-phi_predict = kmeans.predict(dfAlex)
+phi_predict = kmeans.predict(dfNorm)
 
 centers = kmeans.cluster_centers_
-score = kmeans.score(dfAlex)
+score = kmeans.score(dfNorm)
 
 # Compute Clustering Metrics
 n_clusters_ = len(centers)
@@ -46,7 +52,7 @@ print("Adjusted Rand Index: %0.3f"
 print("Adjusted Mutual Information: %0.3f"
       % metrics.adjusted_mutual_info_score(phi_true, phi_predict))
 print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(dfAlex, phi_predict, metric='sqeuclidean'))
+      % metrics.silhouette_score(dfNorm, phi_predict, metric='sqeuclidean'))
 
 # timeit statement
 elapsed = timeit.default_timer() - start_time
@@ -54,14 +60,21 @@ print('Execution time: {0:.4f} sec'.format(elapsed))
 
 # %% 2-D Plot 1
 
-rows = dfAlex.shape[0]
+rows = dfNorm.shape[0]
 
-matAlex = dfAlex.as_matrix()
+# Create matrix out of normalized dataframe
+matNorm = dfNorm.as_matrix()
 
+# Re-Create dfNHTS 
+dfNHTS = df0.filter(colNames, axis = 1)
+dfNHTS = dfNHTS.head(numTrips) # shows first n rows
+matNHTS = dfNHTS.as_matrix()
+
+# Find min distance between dfNorm domain and centers
 dst = np.zeros((rows,clusters))
 row = 0
 col = 0
-for pt in matAlex:
+for pt in matNorm:
     col = 0
     for c in centers:
             dst[row][col] = distance.euclidean(pt,c)
@@ -69,25 +82,30 @@ for pt in matAlex:
     row += 1
 
 idx = np.argmin(dst, axis = 0)
+plotCentersReal = np.zeros((clusters,2))
 centersReal = np.zeros((clusters,2))
 j = 0
 
 for i in idx:
-    centersReal[j] = matAlex[i]
+    plotCentersReal[j] = matNorm[i]
+    centersReal[j] = matNHTS[i]
     j += 1
-    
-    #np.argmin(a, axis=0)
 
-fig = plt.figure()
+
 kMX_plot = plt.figure() 
 
 #plt.grid(True, which='both')
 '''
-plt.scatter(matAlex[:,0],matAlex[:,1])
+plt.scatter(matNorm[:,0],matNorm[:,1])
 plt.scatter(centers[:,0],centers[:,1],marker='o')
 plt.scatter(centersReal[:,0],centersReal[:,1],c='w',marker='*',s=10)
 print('k-Means')
 '''
+# %% Create centers dataframes
+
+d = {colNames[0]: centersReal[:,0], colNames[1]: centersReal[:,1]}
+dfCenters = pd.DataFrame(data=d)
+
 # %% 2-D Plot 2
 
 k1 = []
@@ -96,7 +114,7 @@ k3 = []
 k4 = []
 k5 = []
 
-for j in range(0,len(dfAlex.index)):
+for j in range(0,len(dfNorm.index)):
     if phi_true[j] == 0:
         k1.append(j)
     if phi_true[j] == 1:
@@ -108,11 +126,13 @@ for j in range(0,len(dfAlex.index)):
     if phi_true[j] == 4:
         k5.append(j)
 
-dfk1 = dfAlex.filter(k1, axis = 0)
-dfk2 = dfAlex.filter(k2, axis = 0)
-dfk3 = dfAlex.filter(k3, axis = 0)
-dfk4 = dfAlex.filter(k4, axis = 0)
-dfk5 = dfAlex.filter(k5, axis = 0)
+dfk1 = dfNorm.filter(k1, axis = 0)
+dfk2 = dfNorm.filter(k2, axis = 0)
+dfk3 = dfNorm.filter(k3, axis = 0)
+dfk4 = dfNorm.filter(k4, axis = 0)
+dfk5 = dfNorm.filter(k5, axis = 0)
+
+plt.figure(figsize=(8,6))
 
 for k in range(len(k1)):
     row = k1[k]
@@ -134,67 +154,14 @@ for k in range(len(k5)):
     row = k5[k]
     plt.scatter(dfk5.loc[row][0],dfk5.loc[row][1],c='k',marker=".")
 
+# Plot k-Mean Centers
 plt.scatter(centers[:,0],centers[:,1],marker='v')
-plt.scatter(centersReal[:,0],centersReal[:,1],c='w',marker='*',s=5)
+
+# Plot Real Centers (centers in domain)
+plt.scatter(plotCentersReal[:,0],plotCentersReal[:,1],c='w',marker='*',s=5)
 
 plt.title('2-D Cluster')
-plt.xlabel('STRTTIME')
-plt.ylabel('WHYTRP1S')
+plt.xlabel(colNames[0])
+plt.ylabel(colNames[1])
 
-# %% 3-D Plot
-'''
-k1 = []
-k2 = []
-k3 = []
-k4 = []
-k5 = []
 
-for j in range(0,len(dfAlex.index)):
-    if phi_true[j] == 0:
-        k1.append(j)
-    if phi_true[j] == 1:
-        k2.append(j)
-    if phi_true[j] == 2:
-        k3.append(j)
-    if phi_true[j] == 3:
-        k4.append(j)
-    if phi_true[j] == 4:
-        k5.append(j)
-
-dfk1 = dfAlex.filter(k1, axis = 0)
-dfk2 = dfAlex.filter(k2, axis = 0)
-dfk3 = dfAlex.filter(k3, axis = 0)
-dfk4 = dfAlex.filter(k4, axis = 0)
-dfk5 = dfAlex.filter(k5, axis = 0)
-
-fig = plt.figure()
-threedee = plt.figure().gca(projection='3d')
-
-plt.grid(True, which='both')
-
-for k in range(len(k1)):
-    row = k1[k]
-    threedee.scatter(dfk1.loc[row][0],dfk1.loc[row][1],dfk1.loc[row][2],c='r',marker=".")
-
-for k in range(len(k2)):
-    row = k2[k]
-    threedee.scatter(dfk2.loc[row][0],dfk2.loc[row][1],dfk2.loc[row][2],c='g',marker=".")
-
-for k in range(len(k3)):
-    row = k3[k]
-    threedee.scatter(dfk3.loc[row][0],dfk3.loc[row][1],dfk3.loc[row][2],c='b',marker=".")
-
-for k in range(len(k4)):
-    row = k4[k]
-    threedee.scatter(dfk4.loc[row][0],dfk4.loc[row][1],dfk4.loc[row][2],c='m',marker=".")
-
-for k in range(len(k5)):
-    row = k5[k]
-    threedee.scatter(dfk5.loc[row][0],dfk5.loc[row][1],dfk5.loc[row][2],c='k',marker=".")
-
-threedee.set_xlabel('STRTTIME')
-threedee.set_ylabel('TRVL_MIN')
-threedee.set_zlabel('WHYTRP1S')
-
-plt.show()
-'''
